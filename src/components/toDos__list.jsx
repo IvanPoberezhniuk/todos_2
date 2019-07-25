849d4525c3620d0f63b8bf9cef0d1243897a71cf
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-07-25 20:25:27 +0300

    fix overflow and checkboxes in Firefox & EDGE

--- src/components/toDos__list.jsx
+++ src/components/toDos__list.jsx
@@ -4,13 +4,14 @@ let ToDosList = ({ data, toggleIsCompleted, removeTask }) => {
   return data.map(task => {
     return (
       <div className="toDos__dropDownTask" key={task.id}>
-        <div>
+        <div className="toDos__task-wrapper">
           <input
             onClick={() => toggleIsCompleted(task.id)}
             type="checkbox"
             className="toDos__toggle"
             defaultChecked={task.isCompleted}
           />
+          <span className="toDos__toggle--helper" />
           <span className="toDos__task">{task.task}</span>
         </div>
         <button
